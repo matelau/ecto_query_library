811ac5a77d42cb5c951b904578e556504ed7cee6
Asa Matelau <matelau@gmail.com>
2019-12-01 15:55:14 -0700

    upgrade to elixir 1.9 and ecto 3.2, also ran mix format on several files.

--- lib/fishing_spot/repo.ex
+++ lib/fishing_spot/repo.ex
@@ -1,3 +1,11 @@
 defmodule FishingSpot.Repo do
-  use Ecto.Repo, otp_app: :fishing_spot
+  use Ecto.Repo, otp_app: :fishing_spot, adapter: Ecto.Adapters.Postgres
+
+  @doc """
+  Dynamically loads the repository url from the
+  DATABASE_URL environment variable.
+  """
+  def init(_, opts) do
+    {:ok, Keyword.put(opts, :url, System.get_env("DATABASE_URL"))}
+  end
 end
--- mix.exs
+++ mix.exs
@@ -2,21 +2,22 @@ defmodule FishingSpot.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :fishing_spot,
-     version: "0.0.1",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps(),
-     aliases: aliases()]
+    [
+      app: :fishing_spot,
+      version: "0.0.1",
+      elixir: "~> 1.9",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      aliases: aliases()
+    ]
   end
 
   # Configuration for the OTP application
   #
   # Type `mix help compile.app` for more information
   def application do
-    [mod: {FishingSpot, []},
-     extra_applications: [:logger]]
+    [mod: {FishingSpot, []}, extra_applications: [:logger]]
   end
 
   # Dependencies can be Hex packages:
@@ -29,9 +30,7 @@ defmodule FishingSpot.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    [{:postgrex, "~> 0.13"},
-     {:ecto, "~> 2.2"},
-     {:ex_doc, "~> 0.12", only: :dev}]
+    [{:ecto_sql, "~> 3.2"}, {:ex_doc, "~> 0.21.2", only: :dev}, {:postgrex, "~> 0.15.1"}]
   end
 
   defp aliases do
