4e0b592f91d6b5b0316c107a4f26521c84fdf2b0
Asa Matelau <matelau@gmail.com>
2019-12-01 16:06:47 -0700

    removed adapter from config

--- config/test.exs
+++ config/test.exs
@@ -2,7 +2,6 @@ use Mix.Config
 
 config :fishing_spot, FishingSpot.Repo,
   pool: Ecto.Adapters.SQL.Sandbox,
-  adapter: Ecto.Adapters.Postgres,
   database: "fishing_spot_test",
   username: "postgres",
   password: "postgres"
