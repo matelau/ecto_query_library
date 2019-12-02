22843b41703eddb52392a42e6aa29cf564fdedd2
Asa Matelau <matelau@gmail.com>
2019-12-01 20:04:20 -0700

    add select tests

--- lib/fishing_spot/contexts/fish.ex
+++ lib/fishing_spot/contexts/fish.ex
@@ -5,6 +5,11 @@ defmodule FishingSpot.Context.Fish do
   alias FishingSpot.{Fisherman, Repo}
   import Ecto.Query
 
+  def get_fisherman(fisherman_id) do
+    query = from(f in Fisherman, where: f.id == ^fisherman_id)
+    Repo.one(query)
+  end
+
   def get_fisherman_name(fisherman_id) do
     query = from(f in Fisherman, where: f.id == ^fisherman_id, select: f.name)
     Repo.one(query)
@@ -12,6 +17,21 @@ defmodule FishingSpot.Context.Fish do
 
   def get_fisherman_name_and_date_of_birth(fisherman_id, :tuple) do
     query = from(f in Fisherman, where: f.id == ^fisherman_id, select: {f.name, f.date_of_birth})
+    Repo.one(query)
+  end
+
+  def get_fisherman_name_and_date_of_birth(fisherman_id, :map) do
+    query =
+      from(f in Fisherman,
+        where: f.id == ^fisherman_id,
+        select: %{name: f.name, dob: f.date_of_birth}
+      )
+
+    Repo.one(query)
+  end
+
+  def get_fisherman_name_and_date_of_birth(fisherman_id, :list) do
+    query = from(f in Fisherman, where: f.id == ^fisherman_id, select: [f.name, f.date_of_birth])
     Repo.one(query)
   end
 
--- test/fisherman_test.exs
+++ test/fisherman_test.exs
@@ -8,6 +8,13 @@ defmodule FishingSpot.FishermanTest do
   alias FishingSpot.Context.Fish
 
   describe "FishermanEctoTests" do
+    test "Get Fisherman" do
+      fisherman = insert(:fisherman)
+      # Update context to return proper result
+      result = Fish.get_fisherman(fisherman.id)
+      assert result.name == "Fisherman"
+    end
+
     test "Select Fisherman Name" do
       fisherman = insert(:fisherman)
       # Update context to return proper result
@@ -33,6 +40,20 @@ defmodule FishingSpot.FishermanTest do
       # Update context to return proper result
       result = Fish.get_fisherman_name_and_date_of_birth(fisherman.id, :tuple)
       assert result == {"name", ~D[2000-10-01]}
+    end
+
+    test "select fisherman name and date of birth map" do
+      fisherman = insert(:fisherman, name: "name", date_of_birth: "2000-10-01")
+      # Update context to return proper result
+      result = Fish.get_fisherman_name_and_date_of_birth(fisherman.id, :map)
+      assert result == %{dob: ~D[2000-10-01], name: "name"}
+    end
+
+    test "select fisherman name and date of birth list" do
+      fisherman = insert(:fisherman, name: "name", date_of_birth: "2000-10-01")
+      # Update context to return proper result
+      result = Fish.get_fisherman_name_and_date_of_birth(fisherman.id, :list)
+      assert result == ["name", ~D[2000-10-01]]
     end
   end
 end
