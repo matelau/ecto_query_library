f09f3f45f4329bd2f6e37ae8a3c0d784e7a82042
Asa Matelau <matelau@gmail.com>
2019-12-01 19:36:43 -0700

    add basic tests

--- lib/fishing_spot/contexts/fish.ex
+++ lib/fishing_spot/contexts/fish.ex
@@ -6,7 +6,12 @@ defmodule FishingSpot.Context.Fish do
   import Ecto.Query
 
   def get_fisherman_name(fisherman_id) do
-    query = from(f in Fisherman, where: f.id == ^fisherman_id)
+    query = from(f in Fisherman, where: f.id == ^fisherman_id, select: f.name)
     Repo.one(query)
+  end
+
+  def get_unique_fishermen_names() do
+    query = from(f in Fisherman, select: f.name, distinct: f.name)
+    Repo.all(query)
   end
 end
--- test/fisherman_test.exs
+++ test/fisherman_test.exs
@@ -5,14 +5,27 @@ defmodule FishingSpot.FishermanTest do
 
   use FishingSpot.DataCase
   import EctoQueryLibray.Factory
- alias FishingSpot.Context.Fish
+  alias FishingSpot.Context.Fish
 
   describe "FishermanEctoTests" do
     test "Select Fisherman Name" do
-        fisherman = insert(:fisherman)
-        # Update context to return proper result
-        result = Fish.get_fisherman_name(fisherman.id)
-        assert result == "Fisherman"
+      fisherman = insert(:fisherman)
+      # Update context to return proper result
+      result = Fish.get_fisherman_name(fisherman.id)
+      assert result == "Fisherman"
+    end
+
+    test "Select Unique Fishermen Names" do
+      insert(:fisherman, name: "test")
+      insert(:fisherman, name: "test")
+      insert(:fisherman, name: "test")
+      insert(:fisherman, name: "test")
+      insert(:fisherman, name: "something_unique")
+      # Update context to return proper result
+      results = Fish.get_unique_fishermen_names()
+      # assert Enum.count(results) == 1
+      result = List.first(results)
+      assert result == "something_unique"
     end
   end
 end
